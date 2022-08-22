9e5a5cfcc19a1f55c4cac9595e6b8a3c0f470bff
lacmansoftware <lacmansoftware@gmail.com>
2022-08-22 00:42:37 -0400

    feat: websocket broadcast

--- show_time.py
+++ show_time.py
@@ -5,17 +5,27 @@ import datetime
 import random
 import websockets
 
+CONNECTIONS = set()
 
-async def show_time(websocket):
+
+async def register(websocket):
+    CONNECTIONS.add(websocket)
+    try:
+        await websocket.wait_closed()
+    finally:
+        CONNECTIONS.remove(websocket)
+
+
+async def show_time():
     while True:
         message = datetime.datetime.utcnow().isoformat() + "Z"
-        await websocket.send(message)
+        websockets.broadcast(CONNECTIONS, message)
         await asyncio.sleep(random.random() * 2 + 1)
 
 
 async def main():
-    async with websockets.serve(show_time, "localhost", 5678):
-        await asyncio.Future()  # run forever
+    async with websockets.serve(register, "localhost", 5678):
+        await show_time()
 
 if __name__ == "__main__":
     asyncio.run(main())
